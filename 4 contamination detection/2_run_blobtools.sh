1aea266142d3e22b45176022340f814db3f78d38
weiwen wang <u4987172@anu.edu.au>
2019-05-23 14:28:04 +1000

    Create 2_run_blobtools.sh

---
+++ 4 contamination detection/2_run_blobtools.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+inputGenome='genome.fa'
+outdir="/data/snowgum_assembly/blobtools/"
+taxid="/data/db/taxid"
+coverage_file ="mapping.sort.bam"
+hit_file = "last.taxified.out"
+
+
+cd $outdir
+
+echo "creating blobDB file"
+# creating a blob database file from the taxified  hit file
+blobtools create -i $assembly -b $coverage_file -t $hit_file -o $outdir/"genome"
+
+echo "creating blobplot"
+# There  will be two plots  one is G_C coverage plot and another one is taxid mapping plot
+blobtools plot  -i  $outdir/"genome.blobDB.json" -o $outdir
