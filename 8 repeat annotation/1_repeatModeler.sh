bd8ee3d77564b0a1d7614039d6784884215ba901
weiwen wang <u4987172@anu.edu.au>
2019-05-23 16:20:34 +1000

    Update 1_repeatModeler.sh

--- 8 repeat annotation/1_repeatModeler.sh
+++ 8 repeat annotation/1_repeatModeler.sh
@@ -4,6 +4,7 @@
 set -e
 
 ###################################################################################
+REPEATMODELER_HOME='/home/raymond/devel/repeatMasker/repeatModeler/RepeatModeler-open-1.0.11/'
 speciesName='Epau'
 reference='genome.polish.purge.fa'
 #use 4 per one threads, if set it to 10, it will use 40
@@ -11,12 +12,10 @@ threads=10
 ###################################################################################
 
 
-cd 01_repeatModeler
-rm -fr *
-/home/raymond/devel/repeatMasker/repeatModeler/RepeatModeler-open-1.0.11/BuildDatabase \
+$REPEATMODELER_HOME/BuildDatabase \
     -name $speciesName -engine ncbi $reference 2>&1 |tee buildDB.log
 
-/home/raymond/devel/repeatMasker/repeatModeler/RepeatModeler-open-1.0.11/RepeatModeler \
+$REPEATMODELER_HOME/RepeatModeler \
     -engine ncbi -pa 10 -database $speciesName 2>&1 |tee repeatModeler.log
 
 rm -r RM_*/round-*
